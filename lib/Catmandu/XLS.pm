85750862496864738f2899c674d997c7fe4a6754
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2013-09-12 11:58:30 +0000

    Import of NICS/Catmandu-XLS-0.03 from CPAN.
    gitpan-cpan-distribution: Catmandu-XLS
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         NICS/Catmandu-XLS-0.03.tar.gz
    gitpan-cpan-author:       NICS
    gitpan-cpan-maturity:     released

--- lib/Catmandu/XLS.pm
+++ lib/Catmandu/XLS.pm
@@ -6,11 +6,11 @@ Catmandu::XLS - modules for working with Excel .xls files
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 MODULES
 
